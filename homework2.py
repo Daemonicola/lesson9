dc5ccbbd3784c2b53df07c7769efea312f3d1c0a
Vladimir <168404596+Daemonicola@users.noreply.github.com>
2024-05-07 13:51:11 -0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,29 @@
+x = 38
+print('дратути!')
+if x < 0:
+    print('Меньше нуля')
+print('дотвидания!')
+
+a, b =645, 436
+if a > b:
+    print('a > b')
+if a > b and a > 0:
+    print('Ok')
+if (a > b) and (a > 0 or b < 1000):
+    print('Ok')
+if 436 < b and b < 645:
+    print('Ok')
+
+if '34' > '123':
+    print('Ok')
+if '123' > '12':
+    print('Ok')
+if [1,2] > [1,1]:
+    print('Ok')
+
+#if '6' > 5:
+   # print('Ok')
+#if [5,6] > 5:
+    #print('Ok')
+if '6' != 5:
+    print('Ok')
